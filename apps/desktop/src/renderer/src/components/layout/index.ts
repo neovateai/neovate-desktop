483daf8cae3feecc466559059302251b21be3e73
dinq <iamdinq@gmail.com>
2026-02-28 14:22:28 +0800

    feat: add layout barrel exports

---
+++ apps/desktop/src/renderer/src/components/layout/index.ts
@@ -0,0 +1,15 @@
+export { useLayoutStore } from "./use-layout-store"
+export {
+  AppLayoutRoot,
+  AppLayoutPrimarySidebar,
+  AppLayoutTitleBar,
+  AppLayoutChatPanel,
+  AppLayoutContentPanel,
+  AppLayoutSecondarySidebar,
+  AppLayoutActivityBar,
+} from "./app-layout"
+export { TrafficLightsSection } from "./traffic-lights"
+export { PrimaryTitleBar } from "./primary-titlebar"
+export { SecondaryTitleBar } from "./secondary-titlebar"
+export { ActivityBar } from "./activity-bar"
+export { StatusBar } from "./status-bar"
