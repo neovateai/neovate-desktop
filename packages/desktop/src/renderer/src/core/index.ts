cce9cc5e371ba56f32da15afc7f56a6b2f6c7306
dinq <iamdinq@gmail.com>
2026-03-02 02:01:41 +0800

    feat: add core barrel export

---
+++ packages/desktop/src/renderer/src/core/index.ts
@@ -0,0 +1,16 @@
+export { RendererApp, useRendererApp } from "./app";
+export type { RendererAppOptions } from "./app";
+export type {
+  RendererPlugin,
+  RendererPluginHooks,
+  PluginContext,
+  PluginContributions,
+  ActivityBarItem,
+  SidebarPanel,
+  ContentPanel,
+  PluginTab,
+  TitlebarItem,
+} from "./plugin";
+export { PluginManager } from "./plugin";
+export type { Disposable } from "./disposable";
+export { DisposableStore, toDisposable } from "./disposable";
