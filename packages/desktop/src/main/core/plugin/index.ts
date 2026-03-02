69bd2e163bb21d9b5d32872e48f51f9db8bd399b
dinq <iamdinq@gmail.com>
2026-03-02 17:35:17 +0800

    feat: add core/plugin barrel export

---
+++ packages/desktop/src/main/core/plugin/index.ts
@@ -0,0 +1,9 @@
+export { PluginManager } from "./plugin-manager";
+export { buildContributions, EMPTY_CONTRIBUTIONS } from "./contributions";
+export type { Contributions } from "./contributions";
+export type {
+  MainPlugin,
+  MainPluginHooks,
+  PluginContributions,
+  PluginContext,
+} from "./types";
