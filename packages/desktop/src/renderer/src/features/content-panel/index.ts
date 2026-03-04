f5f605feacb33032771b3ac83c3c2ec83dc2965d
dinq <iamdinq@gmail.com>
2026-03-04 10:54:09 +0800

    feat: add content-panel barrel export

---
+++ packages/desktop/src/renderer/src/features/content-panel/index.ts
@@ -0,0 +1,4 @@
+export { ContentPanel } from "./content-panel";
+export type { ContentPanelHooks } from "./content-panel";
+export type { Tab, ProjectTabState, ContentPanelStoreState } from "./types";
+export { ViewContextProvider, useInstanceId, useViewState } from "./view-context";
