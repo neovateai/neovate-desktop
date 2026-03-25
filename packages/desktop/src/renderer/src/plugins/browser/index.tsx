46460f8ceed883e29fd7daf18879e5bce759759f
dinq <iamdinq@gmail.com>
2026-03-25 07:27:56 -0400

    fix: align DEFAULT_HOSTS with VS Code simple-browser

--- packages/desktop/src/renderer/src/plugins/browser/index.tsx
+++ packages/desktop/src/renderer/src/plugins/browser/index.tsx
@@ -10,10 +10,9 @@ const DEFAULT_HOSTS = new Set([
   "localhost",
   "127.0.0.1",
   "[::1]",
-  "[0:0:0:0:0:0:0:1]",
   "0.0.0.0",
-  "[::]",
   "[0:0:0:0:0:0:0:0]",
+  "[::]",
 ]);
 
 interface BrowserPluginOptions {
