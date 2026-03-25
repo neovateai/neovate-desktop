6d70ba712a06abccb399f515fb6c16b789352bf3
dinq <iamdinq@gmail.com>
2026-03-25 07:19:30 -0400

    fix: restrict includeHosts to localhost and 127.0.0.1

--- packages/desktop/src/renderer/src/main.tsx
+++ packages/desktop/src/renderer/src/main.tsx
@@ -4,6 +4,6 @@ import { RendererApp } from "./core";
 import browserPlugin from "./plugins/browser";
 
 const app = new RendererApp({
-  plugins: [browserPlugin({ includeHosts: ["*"] })],
+  plugins: [browserPlugin({ includeHosts: ["localhost", "127.0.0.1"] })],
 });
 app.start();
